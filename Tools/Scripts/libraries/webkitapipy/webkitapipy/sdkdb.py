6131e829e5a7655bd1b6c5017cde8b5464c49ff2
Elliott Williams <emw@apple.com>
2025-11-10 15:35:35 -0800

    [webkitapipy] Support conditional allowlist entries based on OS / SDK version https://bugs.webkit.org/show_bug.cgi?id=302293 rdar://164435576
    Reviewed by NOBODY (OOPS!).
    
    There are some cases where a more general OS or SDK version check is
    superior to conditionalizing an allowlist entry based on a
    wtf/Platform.h flag:
    
    - Staged API adoption is based on forward-declaring API that has yet to
      appear in the builds of the SDK. Until it does, it will look like SPI
      to the tool. But its usability (and whether or not it is API) is not based
      on a WebKit feature flag, it's based on the SDK vesrion.
    
    - API from frameworks that WebKit builds with internally (such as
      AuthenticationServices.framework) may have already shipped in new
      SDKs, but when building against older SDKs appears as SPI. Again, the
      SDK is what determines whether the usage looks like SPI.
    
    To support these use cases, add optional "request-os" and "requires-sdk" version
    requirement keys to the allowlist format. Each is a list of strings with
    the form "<platform> <operator> <version>". For example:
    
        [[staging]]
        classes = [MyNewClass]
        requires = [ENABLE_MY_NEW_FEATURE]
        requires-sdk = ["iOS < 26.2"]
    
    In the SDKDB cache, this is implemented by extending the existing
    "conditional chain" mechanism used for Platform flags. Conditionals now
    have a value (WTF-style defines are all value "1"), and each link in the
    chain has an associated operator.
    
    * Tools/Scripts/libraries/webkitapipy/webkitapipy/allow.py:
    (AllowedSPI):
    (AllowedSPI.RequiredVersion):
    (AllowList.from_dict):
    
    * Tools/Scripts/libraries/webkitapipy/webkitapipy/allow_unittest.py:
    (TestAllowList.test_repeated_requirements):
    (TestAllowList.test_no_string):
    (TestAllowList):
    (TestAllowList.test_invalid_version_requirements):
    (TestAllowList.test_required_fields):
    
    * Tools/Scripts/libraries/webkitapipy/webkitapipy/macho.py: Read the
      platform and versions from each binary's Mach-O header.
    (APIReport):
    (APIReport.from_binary):
    (APIReport._populate_from_dyld_info):
    * Tools/Scripts/libraries/webkitapipy/webkitapipy/program.py:
    * Tools/Scripts/libraries/webkitapipy/webkitapipy/sdkdb.py:
    (apply_operator): Custom scalar function hooked into sqlite to apply
     the operator in a cond_chain row. This could be written natively in
     the query using nested if statements, but would be unwieldy and
     repetitive. The calls to this function scale with the number of
     conditionals (not overall API usage), so I'm not too worried about
     performance impact.
    (semver_to_int): Used to store version numbers as 6-digit integers, for
      easy comparison, similar to what OS headers do.
    (SDKDB.__init__):
    (SDKDB._initialize_db):
    (SDKDB._initialize_temporary_schema):
    (SDKDB._add_allowlist):
    (SDKDB.add_conditions): New entrypoint for add_defines which takes
      key-value pairs instead of just keys.
    (SDKDB.add_for_auditing): Read the platform and versions of the binaries
      being analyzed, and store them in the cache similar to active
      compilation conditions.
    (SDKDB.audit):
    (SDKDB.add_defines): Deleted.
    
    * Tools/Scripts/libraries/webkitapipy/webkitapipy/sdkdb_unittest.py:
    (TestSDKDB.test_entries_removed_when_binary_updated):
    (TestSDKDB.test_audit_allowed_conditional):
    (TestSDKDB.test_audit_missing_name_conditional):
    (TestSDKDB.test_audit_missing_name_negated_conditional):
    (TestSDKDB.test_audit_allowed_negated_conditional):
    (TestSDKDB.test_audit_allowed_multiple_conditions):
    (TestSDKDB.test_audit_missing_name_multiple_conditions):
    (TestSDKDB.test_audit_missing_name_multiple_conditions_negation):
    (TestSDKDB):
    (TestSDKDB.test_audit_minos_conditions):
    (TestSDKDB.test_audit_api_in_loaded_and_unloaded_library):

--- Tools/Scripts/libraries/webkitapipy/webkitapipy/sdkdb.py
+++ Tools/Scripts/libraries/webkitapipy/webkitapipy/sdkdb.py
@@ -26,6 +26,7 @@ from __future__ import annotations
 import json
 import os
 import sqlite3
+import sys
 from enum import Enum
 from fnmatch import fnmatch
 from typing import Any, Callable, Iterable, NamedTuple, Optional, Union
@@ -37,7 +38,7 @@ from .allow import AllowList
 
 # Increment this number to force clients to rebuild from scratch, to
 # accomodate schema changes or fix caching bugs.
-VERSION = 8
+VERSION = 9
 
 
 class DeclarationKind(Enum):
@@ -64,6 +65,44 @@ class DeclarationKind(Enum):
 sqlite3.register_adapter(DeclarationKind, DeclarationKind.to_sql)
 sqlite3.register_converter("DeclarationKind", DeclarationKind.from_sql)
 
+PLATFORM = '__PLATFORM'
+OS_VERSION = '__OS_VERSION'
+SDK_VERSION = '__SDK_VERSION'
+
+
+def apply_operator(op, x, y) -> bool:
+    if op == '==':
+        return x == y
+    elif op == '!=':
+        return x != y
+    elif op == '>':
+        return x > y
+    elif op == '>=':
+        return x >= y
+    elif op == '<=':
+        return x <= y
+    elif op == '<':
+        return x < y
+    else:
+        # This function is called by sqlite3's engine, where exceptions fail
+        # the query but do not propagate. Print to stderr to help explain why
+        # the call failed.
+        print(f'unknown comparison operator "{op}"', file=sys.stderr)
+        raise ValueError(op)
+
+
+def semver_to_int(semver: str) -> int:
+    version = tuple(map(int, semver.split('.')))
+    if len(version) > 3 or any(x > 99 for x in version):
+        raise ValueError(f'Semantic version "{semver}" must be no more than '
+                         '3 components, with each component below 99')
+    elif len(version) == 3:
+        return version[0] * 10000 + version[1] * 100 + version[2]
+    elif len(version) == 2:
+        return version[0] * 10000 + version[1] * 100
+    else:
+        return version[0] * 10000
+
 
 class MissingName(NamedTuple):
     name: str
@@ -86,6 +125,7 @@ class UnnecessaryAllowedName(NamedTuple):
 
 
 Diagnostic = Union[MissingName, UnusedAllowedName, UnnecessaryAllowedName]
+ConditionVariable = Union[bool, str, int, float]
 
 SYMBOL = DeclarationKind.SYMBOL
 OBJC_CLS = DeclarationKind.OBJC_CLS
@@ -137,6 +177,7 @@ class SDKDB:
                 self.con.close()
                 db_file.unlink()
         self._initialize_temporary_schema()
+        self.con.create_function("ApplyOperator", 3, apply_operator, deterministic=True)
 
     def _initialize_db(self):
         cur = self.con.cursor()
@@ -159,7 +200,7 @@ class SDKDB:
                     '   cond_id, input_file REFERENCES input_file(path) '
                     '                       ON DELETE CASCADE)')
         cur.execute('CREATE INDEX allow_names ON allow (name, kind)')
-        cur.execute('CREATE TABLE condition_chain(name, invert, nextid, '
+        cur.execute('CREATE TABLE condition_chain(name, op Operator, operand, nextid, '
                     '   input_file REFERENCES input_file(path) '
                     '              ON DELETE CASCADE)')
         cur.execute(f'PRAGMA user_version = {VERSION}')
@@ -172,7 +213,7 @@ class SDKDB:
         cur.execute('CREATE TEMPORARY TABLE imports(name, '
                     '   kind DeclarationKind, input_file, arch)')
         cur.execute('CREATE INDEX import_names ON imports(name, kind)')
-        cur.execute('CREATE TEMPORARY TABLE condition(name UNIQUE)')
+        cur.execute('CREATE TEMPORARY TABLE condition(name UNIQUE, value)')
         self.con.commit()
 
     def __del__(self):
@@ -329,6 +370,7 @@ class SDKDB:
     def _add_allowlist(self, config: AllowList, allowlist: Path):
         for entry in config.allowed_spi:
             cond_id = None
+            cur = self.con.cursor()
             if entry.requires:
                 # Convert a requirements list like ["A", "B", "!C"] into a
                 # graph data structure e.g. (A) -> (B) -> (!C). The head node
@@ -339,12 +381,24 @@ class SDKDB:
                 # FIXME: No effort is made to reuse nodes in the graph between
                 # allowlist entries, so we store more than we have to.
                 # (https://bugs.webkit.org/show_bug.cgi?id=295819)
-                cur = self.con.cursor()
-                for req in reversed(entry.requires):
-                    cur.execute('INSERT INTO condition_chain VALUES (?,?,?,?)',
-                                (req.removeprefix('!'), req.startswith('!'),
+                for name in reversed(entry.requires):
+                    cur.execute('INSERT INTO condition_chain VALUES (?,?,?,?,?)',
+                                (name.removeprefix('!'),
+                                 '!=' if name.startswith('!') else '==', 1,
                                  cond_id, str(allowlist.resolve())))
                     cond_id = cur.lastrowid
+            for req in entry.requires_os:
+                cur.execute('INSERT INTO condition_chain VALUES (?,?,?,?,?)',
+                            (f'{OS_VERSION}_{req.platform}', req.operator,
+                             semver_to_int(req.version), cond_id,
+                             str(allowlist.resolve())))
+                cond_id = cur.lastrowid
+            for req in entry.requires_sdk:
+                cur.execute('INSERT INTO condition_chain VALUES (?,?,?,?,?)',
+                            (f'{SDK_VERSION}_{req.platform}', req.operator,
+                             semver_to_int(req.version), cond_id,
+                             str(allowlist.resolve())))
+                cond_id = cur.lastrowid
             for symbol in entry.symbols:
                 self._add_symbol(symbol, allowlist,
                                  dest=self.InsertionKind.ALLOW,
@@ -361,15 +415,22 @@ class SDKDB:
                                         cond_id=cond_id,
                                         allow_unused=entry.allow_unused)
 
-    def add_defines(self, defines: list[str]):
+    def add_conditions(self, conditions: dict[str, ConditionVariable]):
         cur = self.con.cursor()
-        cur.executemany('INSERT INTO condition VALUES (?)',
-                        ((d,) for d in defines))
+        cur.executemany('INSERT INTO condition VALUES (?,?)',
+                        ((k, v) for k, v in conditions.items()))
 
     def add_for_auditing(self, report: APIReport):
         cur = self.con.cursor()
         path = str(report.file.resolve())
         arch = report.arch
+
+        # FIXME: It doesn't really make sense to add these per-report; they should be set once per run.
+        self.add_conditions({
+            f'{OS_VERSION}_{report.platform}': semver_to_int(report.min_os),
+            f'{SDK_VERSION}_{report.platform}': semver_to_int(report.sdk)
+        })
+
         # Don't use _cache_hit_preparing_to_insert to update the window. The
         # imports table is not persisted, and we don't want to prevent a
         # different invocation that reads exports from this binary from
@@ -401,16 +462,14 @@ class SDKDB:
         cur.execute('WITH RECURSIVE active_cond AS ('
                     '   SELECT cc.rowid AS nextid, name '
                     '   FROM condition_chain AS cc NATURAL LEFT JOIN condition '
-                    '   WHERE nextid IS NULL AND iif(cc.invert, '
-                    '                                condition.name IS NULL, '
-                    '                                condition.name IS NOT NULL) '
+                    '   WHERE nextid IS NULL AND ApplyOperator(cc.op, '
+                    '                                          condition.value, '
+                    '                                          cc.operand)'
                     '   UNION ALL '
                     '   SELECT cc.rowid AS nextid, cc.name '
                     '   FROM condition_chain AS cc JOIN active_cond USING (nextid) '
                     '   NATURAL LEFT JOIN condition '
-                    '   WHERE iif(cc.invert, '
-                    '             condition.name IS NULL, '
-                    '             condition.name IS NOT NULL)'
+                    '   WHERE ApplyOperator(cc.op, condition.value, cc.operand)'
                     ') '
                     # Then cross-check imports and allowed declarations against
                     # exports.
